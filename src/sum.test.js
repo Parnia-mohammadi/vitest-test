9e7409f054d2a2920ab8d846be4602b1c5acc5d2
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-11 23:58:41 +0330

    grouping tests

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,8 +1,16 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import sum from "./sum";
 test("sum of any numbers", () => {
-  expect(sum(1, 2,3)).toEqual(6);
+  expect(sum(1, 2, 3)).toEqual(6);
 });
-test("test 5 numbers",()=>{
-    expect(sum(1,2,3,4,5)).toEqual(15);
-})
+test("test 5 numbers", () => {
+  expect(sum(1, 2, 3, 4, 5)).toEqual(15);
+});
+describe("group #1", () => {
+  test("sum of any numbers", () => {
+    expect(sum(1, 2, 3)).toEqual(6);
+  });
+  test("test 5 numbers", () => {
+    expect(sum(1, 2, 3, 4, 5)).toEqual(15);
+  });
+});
