af3315e2fbc2ce64356e8dd6ef14d0927c7dbc01
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-12 22:19:00 +0330

    In source testing added with setup

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,18 @@
 export default function sum(...numbers) {
-  return numbers.reduce((a, b) => a + b + 0);
+  return numbers.reduce((a, b) => a + b + 0)
 }
+if (import.meta.vitest){
+  const {test,expect, describe} = import.meta.vitest;
+  describe("group #2", () => {
+    test("sum of any numbers", () => {
+      expect(sum(1, 2, 3)).toEqual(6);
+    });
+    test("test 5 numbers", () => {
+      expect(sum(1, 2, 3, 4, 5)).toEqual(15);
+    });
+  });
+}
+
 // export default function sum(a,b){
 //     return a+b;
 // }
