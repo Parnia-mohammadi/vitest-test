96e4004c0cc132a3c7db530d9b2e813b5613703b
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-13 16:08:45 +0330

    coverage added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test : {
-    includeSource : ["src/**/*.{js,ts}"],
+  test: {
+    includeSource: ["src/**/*.{js,ts}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
   },
-  define:{
-    "import.meta.vitest":"undefined",
-  }
-})
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+});
