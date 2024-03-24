b6bb4963ce7001dca3043fe303957039606fc9c5
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-24 16:06:06 +0430

    test for checking typing in input

--- src/AddTodo/__test__/AddTodo.test.jsx
+++ src/AddTodo/__test__/AddTodo.test.jsx
@@ -1,9 +1,18 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import AddTodo from "../AddTodo";
 
-test("render input element #1",()=>{
-    render(<AddTodo setTodos={()=>{}} todos={[]}/>);
-    const inputEl = screen.getByLabelText("todo");
-    expect(inputEl).toBeInTheDocument();
+const mockedSetTodos = vi.fn();
+
+test("render input element #1", () => {
+  render(<AddTodo setTodos={() => {}} todos={[]} />);
+  const inputEl = screen.getByLabelText("todo");
+  expect(inputEl).toBeInTheDocument();
+});
+test("should be able to type in input #2", () => {
+  render(<AddTodo setTodos={mockedSetTodos} todos={[]} />);
+  const inputEl = screen.getByLabelText("todo");
+  fireEvent.click(inputEl);
+  fireEvent.change(inputEl, { target: { value: "coding" } });
+  expect(inputEl.value).toBe("coding");
 });
