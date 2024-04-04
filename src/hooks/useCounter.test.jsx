15e2c7bebfc20a1cb1a35bc0a9778c8228345d88
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-04-04 23:28:27 +0430

    checking initial value of counter when component is rendered

--- src/hooks/useCounter.test.jsx
+++ src/hooks/useCounter.test.jsx
@@ -7,4 +7,10 @@ describe("useCounter", () => {
     const { result } = renderHook(useCounter);
     expect(result.current.count).toBe(0);
   });
+  test("#2 : should render the same value as initial count", () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: { initialCounter: 10 },
+    });
+    expect(result.current.count).toBe(10);
+  });
 });
