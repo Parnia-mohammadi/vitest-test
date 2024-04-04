578b14f5da040b67a2f65bea710cfbf693b45b9e
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-04-05 02:31:42 +0430

    act utility added for rendering actions in hook

--- src/hooks/useCounter.test.jsx
+++ src/hooks/useCounter.test.jsx
@@ -1,6 +1,7 @@
 import { renderHook } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { useCounter } from "./useCounter";
+import { act } from "react-dom/test-utils";
 
 describe("useCounter", () => {
   test("#1 : should render the initial value", () => {
@@ -12,5 +13,15 @@ describe("useCounter", () => {
       initialProps: { initialCounter: 10 },
     });
     expect(result.current.count).toBe(10);
+  });
+  test("#3 : should increment the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+  test("#4 : should decrement the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 });
