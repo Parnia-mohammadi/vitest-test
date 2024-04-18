2d10cb1f945067741bbef8745511df2fe86ea821
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-04-18 19:57:55 +0430

    pseudoclass selector added in styled component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: ${(props) => (props.varient == "primary" ? "blue" : "#bf4774")};
-  color: ${(props)=>props.varient == "primary"?"white":"black"};
+  background-color: ${(props) =>
+    props.varient == "primary" ? "blue" : "#bf4774"};
+  color: ${(props) => (props.varient == "primary" ? "white" : "black")};
   font-size: 1rem;
   border-radius: 0.5rem;
   padding: 0.5rem;
+  &:hover {
+    background-color: white;
+    color: ${(props) => (props.varient == "primary" ? "blue" : "#bf4774")};
+    border: 1px solid
+    ${(props) => (props.varient == "primary" ? "primary" : "#bf4774")};
+  }
 `;
 export default Button;
